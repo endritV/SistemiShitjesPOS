1cae0a5f8ad4174df4e865400cecd1b3ff8c3207
driart gajtani <driart300@hotmail.com>
2017-04-13 15:18:25 +0200

    Klasat : Artikulli,FaturaDetajet,Sektori

---
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.EntityLayer/Artikulli.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemiShitjesPOS.EntityLayer
+{
+    class Artikulli
+    {
+        private int _idArtukulli;
+        private string _emriArtikullit;
+        private string _barkodi;
+        private string _njesia;
+        private string _pershkrimi;
+        public bool IsAktiv { get; set; }
+
+        public string Pershkrimi
+        {
+            get { return _pershkrimi; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Pershkrimi eshte i zbrazet");
+                }
+                _pershkrimi = value; }
+        }
+
+
+        public string Njesia
+        {
+            get { return _njesia; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Njesia eshte e zbrazet");
+                }
+                _njesia = value; }
+        }
+
+
+        public string Barkodi
+        {
+            get { return _barkodi; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Barkodi eshte i zbrazet");
+                }
+                _barkodi = value; }
+        }
+
+
+        public int IdArtikulli
+        {
+            get { return _idArtukulli; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Artikulli eshte i zbazet");
+                }
+                _idArtukulli = value;
+            }
+        }
+        
+        public string EmriArtikullit
+        {
+            get { return _emriArtikullit; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Emri i Artikullit eshte i zbazet");
+                }
+                _emriArtikullit = value;      
+            }
+                
+        }
+    
+
+
+    }
+}
