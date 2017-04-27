201f4af66c56369a54fbd37e47dc40002ae6cc12
Endrit Vitija <Endrit Vitija@DESKTOP-GI5UP23>
2017-04-27 12:38:35 +0200

    merri

--- SistemiShitjesPOS.UI/SistemiShitjesPOS.UI/DashboardForm.cs
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.UI/DashboardForm.cs
@@ -17,17 +17,17 @@ namespace SistemiShitjesPOS.UI
             InitializeComponent();
         }
 
-        private void btnMenu_Click(object sender, EventArgs e)
-        {
-            if (panMenu.Width == 200)
-            {
-                panMenu.Width = 50;
-            }
-            else
-            {
-                panMenu.Width = 200;
-            }
-        }
+        //private void btnMenu_Click(object sender, EventArgs e)
+        //{
+        //    if (panMenu.Width == 200)
+        //    {
+        //        panMenu.Width = 50;
+        //    }
+        //    else
+        //    {
+        //        panMenu.Width = 200;
+        //    }
+        //}
 
         private void btnSuppliers_Click(object sender, EventArgs e)
         {
@@ -59,19 +59,19 @@ namespace SistemiShitjesPOS.UI
 
         }
 
-        private void btnLogOut_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("Do you want to exit?", "My Application",
-        MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-        == DialogResult.Yes)
-            {
+        //private void btnLogOut_Click(object sender, EventArgs e)
+        //{
+        //    if (MessageBox.Show("Do you want to exit?", "My Application",
+        //MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        //== DialogResult.Yes)
+        //    {
                 
-                LoginForm l1 = new LoginForm();
-                DashboardForm b1 = new DashboardForm();
-                b1.Close();
-                l1.Show();
-            }
-        }
+        //        LoginForm l1 = new LoginForm();
+        //        DashboardForm b1 = new DashboardForm();
+        //        b1.Close();
+        //        l1.Show();
+        //    }
+        //}
     }
     }
 
