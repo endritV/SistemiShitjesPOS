0d949960054addad93e6916ed61514df94ac9edb
driart gajtani <driart300@hotmail.com>
2017-04-27 14:42:06 +0200

    o izet NALEEEE

--- SistemiShitjesPOS.UI/SistemiShitjesPOS.UI/DashBoard.cs
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.UI/DashBoard.cs
@@ -32,8 +32,13 @@ namespace SistemiShitjesPOS.UI
 
         private void btnLogOut_Click(object sender, EventArgs e)
         {
-            LoginForm l1 = new LoginForm();
-            this.Dispose();
+            if (MessageBox.Show("Do you want to exit?", "My Application",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            == DialogResult.Yes)
+            {
+                this.Dispose();
+            }
+            
             
 
         }
