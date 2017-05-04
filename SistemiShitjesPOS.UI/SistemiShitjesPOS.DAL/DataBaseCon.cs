5d1c61361de626d5c085a087e426ca7931ff9e1c
driart gajtani <driart300@hotmail.com>
2017-05-05 00:39:08 +0200

    a

--- SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/DataBaseCon.cs
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/DataBaseCon.cs
@@ -10,13 +10,7 @@ namespace SistemiShitjesPOS.DAL
 {
     class DataBaseCon
     {
-        public SqlConnection con;
-        public void Connection()
-        {
-
-            con = new SqlConnection("Data Source=SQL6001.SmarterASP.NET;Initial Catalog=DB_A1F690_Kisamava;User Id=DB_A1F690_Kisamava_admin;Password=fuckem123;");
-
-        }
+      
         
     }
 }
