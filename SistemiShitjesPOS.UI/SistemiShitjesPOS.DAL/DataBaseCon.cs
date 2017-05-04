09cf9ccaa554e66d1845c23946817d5f1c717323
driart gajtani <driart300@hotmail.com>
2017-05-04 13:28:54 +0200

    constringi

---
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/DataBaseCon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SistemiShitjesPOS.DAL
+{
+    class DataBaseCon
+    {
+        public SqlConnection con;
+        public void Connection()
+        {
+
+            con = new SqlConnection("Data Source=SQL6001.SmarterASP.NET;Initial Catalog=DB_A1F690_Kisamava;User Id=DB_A1F690_Kisamava_admin;Password=fuckem123;");
+
+        }
+        
+    }
+}
