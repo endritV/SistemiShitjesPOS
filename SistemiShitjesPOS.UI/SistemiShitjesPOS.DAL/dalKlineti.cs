f446ba52c155464d5c05dde771a3cb8677d313fd
Endrit Vitija <Endrit Vitija@DESKTOP-GI5UP23>
2017-05-09 00:16:04 +0200

    dsds

--- SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/dalKlineti.cs
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/dalKlineti.cs
@@ -4,43 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SistemiShitjesPOS.EntityLayer;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SistemiShitjesPOS.DataAccessLayer
 {
+    
     public class dalKlineti
     {
-        private static List<Klienti> _listOfKlient = new List<Klienti>();
+        static readonly string cs = DataBaseCon.GetConnectionString();
 
-        public static void Insert(Klienti klienti)
-        {
-            _listOfKlient.Add(klienti);
-        }
+        //public static List<Klienti> GetKlietnById(int klinetId)
+        //{
+        //    List<Klienti> list = new List<Klienti>();
+        //    using (SqlConnection conn = new SqlConnection())
+        //    {
+        //        conn.Open();
+        //        SqlCommand cmd = new SqlCommand("", conn);
+        //        cmd.Parameters.AddWithValue("@_IdKlienti", klinetId);
+        //        cmd.CommandType = CommandType.StoredProcedure;
 
-        public static void Update(Klienti klienti)
-        {
-            foreach (Klienti oldKlient in _listOfKlient)
-            {
-                if (klienti.NrBiznesit == oldKlient.NrBiznesit)
-                {
-                    oldKlient.Emri = klienti.Emri;
-                }
-            }
-        }
+        //        using(IDataReader reader = cmd.ExecuteReader())
+        //        {
+        //            Klienti klienti = new Klienti();
+        //            klienti.IdKlienti = klinetId;
+                    
 
-        public static void Remove(string nrBiznesit)
-        {
-            var cmp = GetSingleKlinet(nrBiznesit);
-            _listOfKlient.Remove(cmp); 
-        }
-
-        public static Klienti GetSingleKlinet(string nrBiznesit)
-        {
-            foreach (Klienti klienti in _listOfKlient)
-            {
-                if (klienti.NrBiznesit == nrBiznesit)
-                    return klienti;
-            }
-            return null;
-        }
+        //        }
+        //    }
+        //}
     }
-}
+}      
+
+
+//public static void Update(Klienti klienti)
+//{
+//    foreach (Klienti oldKlient in _listOfKlient)
+//    {
+//        if (klienti.NrBiznesit == oldKlient.NrBiznesit)
+//        {
+//            oldKlient.Emri = klienti.Emri;
+//        }
+//    }
+//}
+
+//public static void Remove(string nrBiznesit)
+//{
+//    var cmp = GetSingleKlinet(nrBiznesit);
+//    _listOfKlient.Remove(cmp);
+//}
+
+//public static Klienti GetSingleKlinet(string nrBiznesit)
+//{
+//    foreach (Klienti klienti in _listOfKlient)
+//    {
+//        if (klienti.NrBiznesit == nrBiznesit)
+//            return klienti;
+//    }
+//    return null;
+//}
