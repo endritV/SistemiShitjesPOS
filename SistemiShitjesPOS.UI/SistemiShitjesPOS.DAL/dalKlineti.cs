46e31b5e143f8378a0fbc1c33357a49e951356c7
Endrit Vitija <Endrit Vitija@DESKTOP-GI5UP23>
2017-05-09 14:56:21 +0200

    dsdds

--- SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/dalKlineti.cs
+++ SistemiShitjesPOS.UI/SistemiShitjesPOS.DAL/dalKlineti.cs
@@ -18,6 +18,7 @@ namespace SistemiShitjesPOS.DataAccessLayer
         {
             using (SqlConnection conn = new SqlConnection(cs))
             {
+                
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("spShtoKlient", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -31,31 +32,3 @@ namespace SistemiShitjesPOS.DataAccessLayer
         }
     }
 }
-
-
-//public static void Update(Klienti klienti)
-//{
-//    foreach (Klienti oldKlient in _listOfKlient)
-//    {
-//        if (klienti.NrBiznesit == oldKlient.NrBiznesit)
-//        {
-//            oldKlient.Emri = klienti.Emri;
-//        }
-//    }
-//}
-
-//public static void Remove(string nrBiznesit)
-//{
-//    var cmp = GetSingleKlinet(nrBiznesit);
-//    _listOfKlient.Remove(cmp);
-//}
-
-//public static Klienti GetSingleKlinet(string nrBiznesit)
-//{
-//    foreach (Klienti klienti in _listOfKlient)
-//    {
-//        if (klienti.NrBiznesit == nrBiznesit)
-//            return klienti;
-//    }
-//    return null;
-//}
